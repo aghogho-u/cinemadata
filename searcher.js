9e31158f76058c31ca6c083a54486bb2e8049ab7
AGHOGHO UWEJEYAN <auwejeyan@gmail.com>
2019-08-06 21:22:40 +0100

    ready for deploy

--- searcher.js
+++ searcher.js
@@ -1,43 +1,36 @@
 const fetch = require ('node-fetch');
 const cheerio = require ('cheerio');
 const url = 'https://www.nowshowing.com.ng/';
-//const todayMovies = [];
+const todayMovies = [];
+let titleSearch;
+let itemsProcessed = 0;
 
      function  searchMovies (searchTerm) {
-   // const youtubeVideos = [];
-    
    
-    return    fetch(`${url}${searchTerm}`)
+      return  fetch(`${url}${searchTerm}`)
     .then(response =>  response.text())
-    .then( async html => { 
+    .then(  html => { 
 
     
         const $ = cheerio.load(html);
-        const todayMovies = [];
+        const myCardArray = $('.card');
+       // const todayMovies = [];
         const showtimeArray = [];
         const times = [];
-       $('.card').each(async (i, el)=> {
         
+        myCardArray.each(  (i, el)=>  {
+           
            let card = $(el);
            let title = card.find('h2');
            let titleLink = card.find('a');
            let showtime =  card.find('.showtimeBox');
            let timeArray =  card.find('ul>li:nth-child(2)');
-           let titleSearch = titleLink.attr('href');
-           
-           //console.log(todayMovies);
-           
-            const youtubeLinkItem =  await fetch(`${url}${titleSearch}`)
-           .then(response => response.text())
-           .then( html => {
-                const S = cheerio.load(html);
-                let youtubeLink = S('div.card-content>div>div>ul>li>iframe');
-                youtubeLink = youtubeLink.attr('src');                                
-                 return youtubeLink;
-           }).catch((e)=>{console.log(e)})
-                //let showtime =  card.find('.showtimeBox');
-          //  let showtimeArray = [];
-            showtime.each(  (j, el)=> {
+          // let 
+           titleSearch = titleLink.attr('href');
+          
+            const youtubeVideo =  youtubeLinkItem(searchTerm);
+
+           showtime.each(  (j, el)=> {
             let card = $(el);            
             let cinemaArray =  card.find('h3');
             let cinemas = [];
@@ -46,53 +39,61 @@ const url = 'https://www.nowshowing.com.ng/';
                 cinema = ell.text();
                 cinemas.push(cinema);
             })
-//            let timeArray =  card.find('ul>li:nth-child(2)');
-//            let times = [];
+        
             timeArray.each((k, el)=>{
                 let ell = $(el);
                 time = ell.text();
                 times.push(time);
             })
-
-            
-             
             for(let x=0; x<cinemas.length; x++)
                 {
                     showtimeArray.push([cinemas[x], times[x]]);
-                }   
-                
-            
-           
-             
+                } 
            const movie =  { 
                id: i,
                Movie_title: title.text(),
-              // Movie_link: titleSearch,
-              // Showing: showtimeArray,
-               Movie_trailer: youtubeLinkItem
+               Movie_link: titleSearch,
+               Movie_trailer: youtubeVideo,
+               Showing: showtimeArray
            } 
           // console.log(movie);
            todayMovies.push(movie);
+
+     
         })
            
-      console.log("for each.. ", todayMovies)
+     //   console.log("for each.. ", itemsProcessed , myCardArray.length);
+       
+
     })
-  
+    console.log("for each.. ", itemsProcessed , myCardArray.length);  
 
             
-console.log(todayMovies);
-      return  todayMovies;
+// console.log(todayMovies);
+ //while( itemsProcessed == myCardArray.length){
+    // console.log(todayMovies);
+     return todayMovies;
+ //}
     })
-//     .then((todayMovies)=>{
-//         console.log(todayMovies)
-//         return  todayMovies;
-    
-// })
     .catch((err) =>{console.log(err)});
 
-    //return  todayMovies;
+//    return  todayMovies;
 
 }
+
+
+    const youtubeLinkItem = () => {return fetch(`${url}${titleSearch}`)
+           .then(response => response.text())
+           .then( html => {
+                const S = cheerio.load(html);
+                let youtubeLink = S('div.card-content>div>div>ul>li>iframe');
+                youtubeLink = youtubeLink.attr('src');                                
+                itemsProcessed ++;
+                console.log(youtubeLink, itemsProcessed);
+                 return youtubeLink;
+           }).catch((e)=>{console.log(e)})
+        }
+
 
 
 module.exports = {
